46226961e75d17519dd0ffb919709fd485ce02d6
Ahmed Wagih <ahmd.wagih96@gmail.com>
2024-01-02 02:30:20 +0200

    chore: Initial setup for frontend (next.js and tailwind) + created the layout.

---
+++ client/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Footer, Header, Hero } from "@/components";
+
+export const metadata: Metadata = {
+  title: "Cozy Nest",
+  description: "Hotel Booking Web App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        <Hero />
+        <div className="container mx-auto">
+          {/* <SearchBar /> */}
+        </div>
+        <div className="container mx-auto py-10 flex-1">{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ client/components/index.ts
@@ -0,0 +1,4 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Hero from "./Hero";
+export { Header, Footer, Hero };
